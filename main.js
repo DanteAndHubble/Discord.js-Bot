d91af61e8157bea09c41bf213cdc17d3493bbd8e
DanteAndHubble <73189037+DanteAndHubble@users.noreply.github.com>
2020-10-20 09:38:00 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const prefix = '-';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+
+client.on("ready", () => {
+    console.log("OutLand Bot is Online")
+})
+
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args);
+        
+    } else if (command == 'youtube'){
+        client.commands.get('youtube').execute(message, args);
+
+    } else if (command == 'yess'){
+        client.commands.get('YASSSSS').execute(message, args);
+
+    } else if (command == 'usuck'){
+        client.commands.get('usuck').execute(message, args);
+
+    }
+
+       
+});
+
+client.login('NzU2MjM0MjcyNTY5OTUwMjc4.X2O4DA.acaMGUrYTUO0FSV_7BFQBQlJCEE');
+
+
